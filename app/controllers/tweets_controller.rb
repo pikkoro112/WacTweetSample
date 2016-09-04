1fdb4788935c130660d92836753c7a3e0bb9540c
kawabata <kawabata@example.com>
2016-09-04 02:50:06 +0000

    ルート設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
-  def index
-  end
+
+	def index
+		@tweets = Tweet.all
+		@tweet = Tweet.new
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		redirect_to tweets_path
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:body)
+	end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,20 +1,2 @@
 class Tweet < ActiveRecord::Base
-
-	def index
-		@tweets = Tweet.all
-		@tweet = Tweet.new
-	end
-
-	def create
-		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		redirect_to tweets_path
-	end
-
-	private
-
-	def tweet_params
-		params.require.(:tweet).permit(:body)
-	end
-
 end
